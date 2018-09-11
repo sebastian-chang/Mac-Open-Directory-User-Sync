b16e41980ef9c50349dd319c8de4294d66337ea6
ZerumRex <42446475+ZerumRex@users.noreply.github.com>
2018-09-11 15:48:41 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,16 +2,17 @@
 
 brew install rsync ssh-copy-id
 brew upgrade rsync ssh-copy-id
-mv login.sh logout.sh syncUpload.Sh /usr/local/sbin/
-mv com.syncstartup.plist com.synclogout.plist /Library/LaunchAgents/
-mv com.checkfile.plist /Library/LaunchDaemons/
+sudo mv login.sh logout.sh syncUpload.Sh /usr/local/sbin/
+sudo mv com.syncstartup.plist com.synclogout.plist /Library/LaunchAgents/
+sudo mv com.checkfile.plist /Library/LaunchDaemons/
 ssh-keygen -t rsa
+ssh-copy-id -i ~/.ssh/id_rsa.pub tbdadmin@server-04
 sudo mkdir /private/.sync
 sudo chmod -R 777 /private/.sync
 cp ~/.ssh/known_hosts /private/.sync
 cp ~/.ssh/id_rsa /private/.sync
 chmod 604 /private/.sync/id_rsa
-mv /usr/local/sbin/README.txt /private/.sync/
+sudo mv README.txt /private/.sync/
 sudo su << EOF
 mkdir ~/.ssh/
 cp /private/.sync/known_hosts ~/.ssh/
